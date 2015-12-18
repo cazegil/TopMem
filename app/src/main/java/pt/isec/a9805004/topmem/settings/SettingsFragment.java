56af150df4fccff5201f494526c5daf2ff865658
Carlos Gil <KazeGil@Carloss-MacBook-Pro-2.local>
2015-12-18 01:26:30 +0000

    Commit #2

---
+++ app/src/main/java/pt/isec/a9805004/topmem/settings/SettingsFragment.java
@@ -0,0 +1,19 @@
+package pt.isec.a9805004.topmem.settings;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import pt.isec.a9805004.topmem.R;
+
+/**
+ * Created by KazeGil on 18/12/15.
+ */
+public class SettingsFragment extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settingspanel);
+
+    }
+}
--- app/src/main/java/pt/isec/a9805004/topmem/toplevelmenu/TopLevelActivity.java
+++ app/src/main/java/pt/isec/a9805004/topmem/toplevelmenu/TopLevelActivity.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import pt.isec.a9805004.topmem.R;
 import pt.isec.a9805004.topmem.game.GameActivity;
+import pt.isec.a9805004.topmem.settings.SettingsFragment;
 
 /**
  * Created by KazeGil on 17/12/15.
@@ -19,14 +21,25 @@ public class TopLevelActivity extends Activity{
 
     }
 
+
     public void onClickMenu(View v) {
-        //Button btn = (Button) v;
+        Button btn = (Button) v;
 
         //if(btn.getId() == R.id.btnIniciar)
         //Toast.makeText(getApplicationContext(), "Clicaste no Botao",
         //        Toast.LENGTH_LONG).show();
 
-        Intent intent = new Intent(TopLevelActivity.this, GameActivity.class);
-        startActivity(intent);
+        switch (btn.getId()) {
+            case R.id.btnIniciar:
+                startActivity(new Intent(TopLevelActivity.this, GameActivity.class));
+                break;
+            case R.id.btnSettings:
+                startActivity(new Intent(TopLevelActivity.this, SettingsFragment.class));
+                break;
+            case R.id.btnSair:
+                finish();
+                System.exit(0);
+                break;
+        }
     }
 }
